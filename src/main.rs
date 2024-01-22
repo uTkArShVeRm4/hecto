b140acbe8547b7d8d9cf83ac2bd15d0c9012aeae
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2024-01-22 18:13:29 +0530

    Using termion for getting input

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-use std::io::{stdin, stdout, Read};
+use core::panic;
+use std::io::{stdin, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
+fn die(e: std::io::Error) {
+    panic!("Error: {e}");
 }
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
-    for b in stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:#b} \r", b);
-        } else {
-            println!("{:#b} ({})\r", b, c);
-        }
-        if b == to_ctrl_byte('q') {
-            break;
-        }
+    for key in stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?} \r", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
+                }
+                Key::Ctrl('q') => break,
+                _ => println!("{:?} \r", key),
+            },
+            Err(e) => die(e),
+        };
     }
 }
