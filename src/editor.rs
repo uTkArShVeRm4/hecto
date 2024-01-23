443bab90acff82916e9db1d14611e3035a8ff944
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2024-01-23 22:16:52 +0530

    Refactored and cleared screen using escape sequences and termion

--- src/editor.rs
+++ src/editor.rs
@@ -1,37 +1,58 @@
-use std::io::{stdin, stdout};
+use std::io::{self, stdout, Write};
+use termion;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 fn die(e: std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("Error: {e}");
 }
 
-pub struct Editor {}
+#[derive(Default)]
+pub struct Editor {
+    should_quit: bool,
+}
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn default() -> Self {
+        Self { should_quit: false }
+    }
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
-        for key in stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?} \r", c as u8);
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c);
-                        }
-                    }
-                    Key::Ctrl('q') => break,
-                    _ => println!("{:?} \r", key),
-                },
-                Err(e) => die(e),
-            };
+        loop {
+            if let Err(error) = self.refresh_screen() {
+                die(error);
+            }
+            if self.should_quit == true {
+                println!("Goodbye.\r");
+                break;
+            }
+            if let Err(error) = self.process_keypress() {
+                die(error);
+            }
         }
+    }
+
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => self.should_quit = true,
+            _ => (),
+        }
+        Ok(())
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!("\x1b[2J \x1b[1;1H");
+        // print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        io::stdout().flush()
     }
 }
 
-impl Default for Editor {
-    fn default() -> Self {
-        Self {}
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+#![warn(clippy::all, clippy::pedantic)]
 mod editor;
 
 use editor::Editor;
 fn main() {
-    let editor = Editor::default();
+    let mut editor = Editor::default();
     editor.run();
 }
