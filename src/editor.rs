a9ec014e474b8a019a2bd09777ba9e870b702310
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2024-02-02 23:37:24 +0530

    Can save file to disk

--- src/editor.rs
+++ src/editor.rs
@@ -42,7 +42,7 @@ pub struct Editor {
 impl Editor {
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
-        let mut initial_status = String::from("HELP: Ctrl-Q = quit");
+        let mut initial_status = String::from("HELP: Ctrl-S = Save | Ctrl-Q = Quit");
         let document = if args.len() > 1 {
             let file_name = &args[1];
             let doc = Document::open(&file_name);
@@ -82,6 +82,14 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Ctrl('s') => {
+                if self.document.save().is_ok() {
+                    self.status_message =
+                        StatusMessage::from("File Saved Successfully!".to_string());
+                } else {
+                    self.status_message = StatusMessage::from("Error Saving File!".to_string());
+                }
+            }
             Key::Char(c) => {
                 self.document.insert(&self.cursor_position, c);
                 self.move_cursor(Key::Right);
