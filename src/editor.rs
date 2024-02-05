e0eb90d253b634984d19d3050f7ca5a9ddbc7e35
uTkArShVeRm4 <theutkarsh.verma00@gmail.com>
2024-02-05 22:46:53 +0530

    refactoring and handling edge cases

--- src/editor.rs
+++ src/editor.rs
@@ -46,11 +46,10 @@ impl Editor {
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
         let mut initial_status = String::from("HELP: Ctrl-S = Save | Ctrl-Q = Quit");
-        let document = if args.len() > 1 {
-            let file_name = &args[1];
-            let doc = Document::open(&file_name);
-            if doc.is_ok() {
-                doc.unwrap()
+        let document = if let Some(file_name) = args.get(1) {
+            let doc = Document::open(file_name);
+            if let Ok(doc) = doc {
+                doc
             } else {
                 initial_status = format!("ERR: Could not open file: {}", file_name);
                 Document::default()
@@ -195,14 +194,14 @@ impl Editor {
             Key::Up => y = y.saturating_sub(1),
             Key::PageUp => {
                 y = if y > terminal_size {
-                    y - terminal_size
+                    y.saturating_sub(terminal_size)
                 } else {
                     0
                 };
             }
             Key::PageDown => {
                 y = if y.saturating_add(terminal_size) < height {
-                    y + terminal_size as usize
+                    y.saturating_add(terminal_size)
                 } else {
                     height
                 }
@@ -248,9 +247,7 @@ impl Editor {
 
             match Terminal::read_key()? {
                 Key::Backspace => {
-                    if !result.is_empty() {
-                        result.truncate(result.len() - 1);
-                    }
+                    result.truncate(result.len().saturating_sub(1));
                 }
                 Key::Char('\n') => break,
                 Key::Char(c) => {
@@ -295,10 +292,9 @@ impl Editor {
             self.cursor_position.y.saturating_add(1),
             self.document.len()
         );
+        #[allow(clippy::integer_arithmetic)]
         let len = status.len() + line_indicator.len();
-        if width > len {
-            status.push_str(&" ".repeat(width - len));
-        }
+        status.push_str(&" ".repeat(width.saturating_sub(len)));
         status = format!("{}{}", status, line_indicator);
         status.truncate(width);
         Terminal::set_bg_color(STATUS_BG_COLOR);
@@ -320,6 +316,7 @@ impl Editor {
         let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
         let width = self.terminal.size().width as usize;
         let len = welcome_message.len();
+        #[allow(clippy::integer_arithmetic, clippy::integer_division)]
         let padding = width.saturating_sub(len) / 2;
         let spaces = " ".repeat(padding.saturating_sub(1));
         welcome_message = format!("~{}{}", spaces, welcome_message);
@@ -330,15 +327,19 @@ impl Editor {
     fn draw_row(&self, row: &Row) {
         let start = self.offset.x;
         let width = self.terminal.size().width as usize;
-        let end = start + width;
+        let end = start.saturating_add(width);
         let row = row.render(start, end);
         println!("{}\r", row);
     }
+    #[allow(clippy::integer_arithmetic, clippy::integer_division)]
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
         for terminal_row in 0..height {
             Terminal::clear_current_line();
-            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
+            if let Some(row) = self
+                .document
+                .row(self.offset.y.saturating_add(terminal_row as usize))
+            {
                 self.draw_row(row);
             } else if terminal_row == height / 3 && self.document.is_empty() {
                 self.draw_welcome_message();
